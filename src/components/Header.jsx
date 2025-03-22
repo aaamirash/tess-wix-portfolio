e07a43aad320e77b5a90603b332d263dbb3e9882
Avesta Mirashrafi <avesta@pdx.edu>
2025-03-22 04:07:58 +0000

    Style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,15 @@ const Header = () => {
   return (
     <header className="bg-black text-white p-4">
       <nav className="flex justify-between items-center max-w-7xl mx-auto">
-        <div className="text-left">
-          <h1 className="text-3xl font-bold leading-tight">Tess Wix</h1>
-          <p className="text-base">Soprano</p>
+        <div className="text-left" text-5xl>
+	  <Link to="/" className="hover:scale-105 transition-transform duration-200 inline-block">
+  	    <h1 className="text-5xl font-bold leading-tight">Tess Wix</h1>
+	  </Link>
+	  <p className="text-base">Soprano</p>
         </div>
-        <div className="space-x-6" text-2xl>
-          <Link to="/" className="hover:underline">Home</Link>
-          <Link to="/bio" className="hover:underline">Biography</Link>
-          <Link to="/media" className="hover:underline">Media</Link>
+        <div className="space-x-6" text-3xl>
+          <Link to="/bio" className="hover:scale-105 transition-transform duration-200 inline-block">Biography</Link>
+          <Link to="/media" className="hover:scale-105 transition-transform duration-200 inline-block">Media</Link>
         </div>
       </nav>
     </header>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,21 +6,9 @@ import { Link } from 'react-router-dom';
 const LandingPage = () => {
   return (
     <div className="background">
-      <h1 className="text-6xl font-bold mb-4 text-center">
-        Welcome
+      <h1 className="text-8xl font-bold mb-4 text-center">
+	<Link to="/" className="hover:underline">Tess Wix</Link>
       </h1>
-      <p className="text-lg mb-8 text-center max-w-2xl">
-        Something profound about singing
-      </p>
-      <a
-        href="#"
-        className="bg-white text-blue-500 px-6 py-3 rounded-lg font-semibold hover:bg-gray-200"
-      >
-      </a>
-      <div className="landing-links">
-          <Link to="/bio" className="landing-link">Bio</Link>
-          <Link to="/media" className="landing-link">Media</Link>
-        </div>
     </div>
   );
 };
